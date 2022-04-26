65d8a352ff1cf7608403d6ebfcf5fb3fc022f9db
andremoto <95541037+andremoto@users.noreply.github.com>
2022-04-26 01:32:43 -0400

    Create algo_vanity.py

---
+++ algo_vanity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from algosdk import account, mnemonic
+import time
+
+# Desired prefix for address.
+PREFIX = "" # CHANGE TO WHAT PREFIX IS NEEDED // BETTER RESULYS WHEN CHAR IS < 5
+
+# Variables containing Private Key and Address
+PRIVATE_KEY = ""
+ADDRESS = ""
+
+# Keep looping until desired address is found.
+while (not ADDRESS.startswith(PREFIX)):
+    PRIVATE_KEY, ADDRESS = account.generate_account()
+    
+
+print(ADDRESS)
+print(PRIVATE_KEY)
+print(mnemonic.from_private_key(PRIVATE_KEY))
